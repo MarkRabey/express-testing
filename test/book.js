1a9267c3a2a31ffddac3f647a943278867413fa9
Mark Rabey <mark@markrabey.com>
2019-11-29 10:22:11 -0500

    add more tests

--- test/book.js
+++ test/book.js
@@ -51,6 +51,57 @@ describe('Books', () => {
 
           done();
         });
-    })
+    });
+
+    it('it should POST a book', done => {
+      let book = {
+        title: "The Lord of the Rings",
+        author: "J.R.R. Tolkien",
+        year: 1954,
+        pages: 1200,
+      };
+      chai.request(server)
+        .post('/books')
+        .send(book)
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.be.a('object');
+          res.body.should.have.property('message').eql('Book added');
+          res.body.book.should.have.property('title');
+          res.body.book.should.have.property('author');
+          res.body.book.should.have.property('year');
+          res.body.book.should.have.property('pages');
+
+          done();
+        });
+    });
+  });
+
+  describe('/GET/:id', () => {
+    it('it should GET a book with given id', done => {
+      let book = new Book({
+        title: "The Lord of the Rings",
+        author: "J.R.R. Tolkien",
+        year: 1954,
+        pages: 1200,
+      });
+
+      book.save((err, book) => {
+        chai.request(server)
+          .get('/books/' + book.id)
+          .send(book)
+          .end((err, res) => {
+            res.should.have.status(200);
+            res.body.should.be.a('object');
+            res.body.should.have.property('title');
+            res.body.should.have.property('author');
+            res.body.should.have.property('year');
+            res.body.should.have.property('pages');
+            res.body.should.have.property('_id').eql(book.id);
+
+            done();
+          });
+      });
+    });
   });
 });
